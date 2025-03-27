a55afe7364af36deb779a5827a2df09f1ad7355e
ThomasKiszely <thomas@bohn-jacobsen.dk>
2025-03-27 09:33:13 +0100

    first

---
+++ src/main/java/com/thomas/pizzaparadise/PizzaParadiseApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.pizzaparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaParadiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaParadiseApplication.class, args);
+    }
+
+}
